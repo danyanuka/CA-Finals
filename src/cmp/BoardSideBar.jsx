ddd5d6b4c3edab30d8f5431c1143b3fea85f9780
MeitarGiladi <95337588+MeitarGiladi@users.noreply.github.com>
2023-11-15 18:37:39 +0200

    Adding basic components

---
+++ src/cmp/BoardSideBar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom"
+
+
+export function BoardSideBar() {
+
+    return (
+        <div className="board-side-bar">
+            
+        </div>
+    )
+}
---
+++ src/cmp/NavBar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom"
+
+
+export function Workspace() {
+
+    return (
+        <div className="workspace">
+            
+        </div>
+    )
+}
