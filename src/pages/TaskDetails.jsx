43ffcb835eb0b388195b233d39f88dc38fc01c29
MeitarGiladi <95337588+MeitarGiladi@users.noreply.github.com>
2023-11-20 19:58:42 +0200

    taskDetails base

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router-dom"
 
 
 export function TaskDetails() {
+    // const [isCover, setIsCover] = useState(true);  // dummy
+
     function dummy(ev) {
         console.log("dummy: ", ev)
     }
@@ -9,16 +11,27 @@ export function TaskDetails() {
     return (
         <div className="task-details-wrapper">
             <div className="task-details">
-                <header>
-                    colors
-                    header                    
-                </header>
-                <div>
-                    
+                <div className="task-details-header-cover">
+                    botton
+                    cover color
                 </div>
-                <aside>
-
-                </aside>
+                <header className="task-details-header">
+                    <h1>Create backend services</h1>
+                    <h2>in list Backlog-Server</h2>
+                </header>
+                <div className="task-details-main">
+                    <div className="task-details-main-data">
+                        main
+                    </div>
+                    <aside className="task-details-main-sidebar">
+                        <div className="task-details-main-sidebar-prop">
+                            prop
+                        </div>
+                        <div className="task-details-main-sidebar-actions">
+                            actions
+                        </div>
+                    </aside>
+                </div>
             </div>
         </div>
     )
