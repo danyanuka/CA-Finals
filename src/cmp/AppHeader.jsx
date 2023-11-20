fb59dc7b57e10909b23f98585b32b2cb0c800d67
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2023-11-20 19:27:52 +0200

    Style AppHeader

--- src/cmp/AppHeader.jsx
+++ src/cmp/AppHeader.jsx
@@ -1,26 +1,49 @@
 import { NavLink } from "react-router-dom"
 
-
 export function AppHeader() {
 
     return (
-        <div className="nav-bar">
-            <nav>
-                <NavLink className="nav-link" to="/board"> <img src="../../public/logo.svg" alt="logo" />Trello</NavLink>
-                <NavLink className="nav-link" to="">Workspace</NavLink>
-                <NavLink className="nav-link" to="">Recent</NavLink>
-                <NavLink className="nav-link" to="">Starred</NavLink>
-                <NavLink className="nav-link" to="">More</NavLink>
+        <div className="app-navbar">
+            <nav className="nav-links">
+                <NavLink className="nav-link" to="/board">
+                    <img height="20px" width="91px" className="icon-logo" src="../../public/logo.gif" alt="logo" />
+                </NavLink>
+
+                <NavLink className="nav-link" to="">
+                    Workspace
+                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                </NavLink>
+
+                <NavLink className="nav-link" to="">
+                    Recent
+                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                </NavLink>
+
+                <NavLink className="nav-link" to="">
+                    Starred
+                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                </NavLink>
+
+                <NavLink className="nav-link" to="">
+                    More
+                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                </NavLink>
+
+                <NavLink className="nav-link" to="">
+                    <img className="icon-show-more" src="../../public/add.svg" />
+                </NavLink>
+
             </nav>
 
-            {/* add icon url */}
-            <i className="add-navlink-button icon-add"></i>
-
-            <input type="text" className="search-input" placeholder="Search" />
-
-            <button className="notifications-btn" title="Notifications">NotificationsIcon</button>
-            <button className="information-btn" title="Information">InformationIcon</button>
-            <button className="account-btn" title="Account">AccountIcon</button>
+            <div className="nav-buttons">
+                <div className="search-section">
+                    <img className="icon-search" src="../../public/search.svg" />
+                    <input type="text" className="search-input" placeholder="Search" />
+                </div>
+                <div className="notifications-btn" title="Notifications"><img className="icon-notifications" src="../../public/notifications.svg" /></div>
+                <span className="information-btn" title="Information"><img className="icon-information" src="../../public/information.svg" /></span>
+                <span className="account-btn" title="Account"><img className="icon-account" src="../../public/user.png" /></span>
+            </div>
         </div>
     )
 }
--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -5,11 +5,8 @@ import { AppHeader } from "../cmp/AppHeader"
 export function BoardDetails() {
 
     return (
-        <div className="one-element-page-parent">
-            <section className="home one-element-page-child">
-                <p>BoardDetails page</p>
-                <AppHeader />
-            </section>
+        <div className="home">
+            <AppHeader />
         </div>
     )
 }
