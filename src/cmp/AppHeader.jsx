34e41d603ecf353cc8759f1d9b318f3267734b0e
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2023-11-23 20:04:39 +0200

    AppHeader testing

--- src/cmp/AppHeader.jsx
+++ src/cmp/AppHeader.jsx
@@ -1,51 +1,71 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
 export function AppHeader() {
+    const [isOpen, setIsOpen] = useState(false)
+    const [dropdownName, setDropdownName] = useState('')
+
+    function onToggle(dropdownName) {
+        setIsOpen(!isOpen)
+        setDropdownName(dropdownName)
+    }
 
     return (
-        <div className="app-navbar">
-            <nav className="nav-links">
-                <NavLink className="nav-link" to="/">
-                    <img height="20px" width="91px" className="icon-logo" src="../../public/logo.gif" alt="logo" />
-                </NavLink>
+        <>
+            <div className="app-navbar">
+                <nav className="nav-links">
+                    <NavLink className="nav-link" to="/">
+                        <img height="20px" width="91px" className="icon-logo" src="../../public/logo.gif" alt="logo" />
+                    </NavLink>
 
-                <NavLink className="nav-link" to="" title="Workspace">
-                    Workspace
-                    <i className="icon-show-more"></i>
-                </NavLink>
+                    <div className="nav-link" to="" title="Workspace" onClick={(e) => { onToggle(e.target.title) }} >
+                        Workspace
+                        <i className="icon-show-more"></i>
 
-                <NavLink className="nav-link" to="" title="Recent">
-                    Recent
-                    <i className="icon-show-more"></i>
-                </NavLink>
+                        {isOpen &&
+                            <div className="dropdown">
+                                <p>{dropdownName}</p>
+                            </div>
+                        }
+                    </div>
 
-                <NavLink className="nav-link" to="" title="Starred">
-                    Starred
-                    <i className="icon-show-more"></i>
-                </NavLink>
+                    <div className="nav-link" to="" title="Recent" >
+                        Recent
+                        <i className="icon-show-more"></i>
+                    </div>
 
-                <NavLink className="nav-link" to="" title="More">
-                    More
-                    <i className="icon-show-more"></i>
-                </NavLink>
+                    <div className="nav-link" to="" title="Starred" >
+                        Starred
+                        <i className="icon-show-more"></i>
+                    </div>
 
-                <NavLink className="nav-link create" to="" title="Create">
-                    Create
-                </NavLink>
+                    <div className="nav-link" to="" title="More" >
+                        More
+                        <i className="icon-show-more"></i>
+                    </div>
 
-            </nav>
+                    <div className="nav-link create" to="" title="Create" >
+                        Create
+                    </div>
 
-            <div className="nav-buttons">
-                <div className="search-section">
-                    <i className="icon-search"></i>
-                    <input type="text" className="search-input" placeholder="Search" />
+                </nav>
+
+                <div className="nav-buttons">
+                    <div className="search-section">
+                        <i className="icon-search"></i>
+                        <input type="text" className="search-input" placeholder="Search" />
+                    </div>
+                    <i className="icon-notifications notifications-btn" title="Notifications"></i>
+
+                    <i className="icon-info info-btn" title="Information"></i>
+
+                    <i className="icon-account account-btn" title="Account"></i>
                 </div>
-                <i className="icon-notifications notifications-btn" title="Notifications"></i>
-
-                <i className="icon-info info-btn" title="Information"></i>
-
-                <i className="icon-account account-btn" title="Account"></i>
             </div>
-        </div>
+
+
+        </>
     )
 }
+
+
