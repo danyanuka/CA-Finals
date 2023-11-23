c23000d5994faae284006bc9b47a9429d6ef5266
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2023-11-23 18:21:40 +0200

    Styleing AppHeader

--- src/cmp/AppHeader.jsx
+++ src/cmp/AppHeader.jsx
@@ -5,44 +5,46 @@ export function AppHeader() {
     return (
         <div className="app-navbar">
             <nav className="nav-links">
-                <NavLink className="nav-link" to="/board">
+                <NavLink className="nav-link" to="/">
                     <img height="20px" width="91px" className="icon-logo" src="../../public/logo.gif" alt="logo" />
                 </NavLink>
 
-                <NavLink className="nav-link" to="">
+                <NavLink className="nav-link" to="" title="Workspace">
                     Workspace
-                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                    <i className="icon-show-more"></i>
                 </NavLink>
 
-                <NavLink className="nav-link" to="">
+                <NavLink className="nav-link" to="" title="Recent">
                     Recent
-                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                    <i className="icon-show-more"></i>
                 </NavLink>
 
-                <NavLink className="nav-link" to="">
+                <NavLink className="nav-link" to="" title="Starred">
                     Starred
-                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                    <i className="icon-show-more"></i>
                 </NavLink>
 
-                <NavLink className="nav-link" to="">
+                <NavLink className="nav-link" to="" title="More">
                     More
-                    <img className="icon-show-more" src="../../public/show-more.svg" />
+                    <i className="icon-show-more"></i>
                 </NavLink>
 
-                <NavLink className="nav-link" to="">
-                    <img className="icon-show-more" src="../../public/add.svg" />
+                <NavLink className="nav-link create" to="" title="Create">
+                    Create
                 </NavLink>
 
             </nav>
 
             <div className="nav-buttons">
                 <div className="search-section">
-                    <img className="icon-search" src="../../public/search.svg" />
+                    <i className="icon-search"></i>
                     <input type="text" className="search-input" placeholder="Search" />
                 </div>
-                <div className="notifications-btn" title="Notifications"><img className="icon-notifications" src="../../public/notifications.svg" /></div>
-                <span className="information-btn" title="Information"><img className="icon-information" src="../../public/information.svg" /></span>
-                <span className="account-btn" title="Account"><img className="icon-account" src="../../public/user.png" /></span>
+                <i className="icon-notifications notifications-btn" title="Notifications"></i>
+
+                <i className="icon-info info-btn" title="Information"></i>
+
+                <i className="icon-account account-btn" title="Account"></i>
             </div>
         </div>
     )
