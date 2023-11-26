ac80cf189d2dfc0f58daaa12ce2214001f00a5ef
danyanuka <danyanuka@gmail.com>
2023-11-26 20:02:52 +0200

    WIP/ section 3 started/ mastered the rest

---
+++ src/cmp/HomePage/ThirdSection.jsx
@@ -0,0 +1,20 @@
+export function ThirdSection() {
+  return (
+    <div className="section3-container ">
+      <section className="third-section">
+        <div className="section-header">
+          <p>TRELLO IN ACTION</p>
+          <h1>Workflows for any project, big or small</h1>
+        </div>
+        <div>
+          <button>
+            <i className="icon-carousel-right"></i>
+          </button>
+          <button>
+            <i className="icon-carousel-left"></i>
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 import { HomeHeader } from "../cmp/HomePage/HomeHeader";
 import { FirstSection } from "../cmp/HomePage/FirstSection";
 import { SecondSection } from "../cmp/HomePage/SecondSection";
+import { ThirdSection } from "../cmp/HomePage/ThirdSection";
 
 export function Home() {
   return (
     <div className="home-page-container">
       <HomeHeader />
-      <div>
-        <FirstSection />
-        <SecondSection />
-      </div>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
     </div>
   );
 }
