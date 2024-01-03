45250b22a511c58c666e7869b5c4e3f1216c4c89
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2024-01-03 18:32:49 +0200

    add icon task preview

--- src/cmp/TaskPreview.jsx
+++ src/cmp/TaskPreview.jsx
@@ -1,6 +1,9 @@
 
 
 export function TaskPreview({ task }) {
+    let date = new Date(task.dueDate).toString();
+    date = date.split(" ")
+
     return (
         <div className="task-preview">
             <div className="task-header">
@@ -10,6 +13,42 @@ export function TaskPreview({ task }) {
                 <p>{task.title}</p>
             </div>
             <div className="task-actions">
+                <div className="task-icons">
+                    {task.description &&
+                        <i className="icon-description" title="This card has a description"></i>
+                    }
+
+                    {task.comments &&
+                        <div className="icon-with-counts">
+                            <i className="icon-comments" title="Comments"></i>
+                            <span>{task.comments.length + 1}</span>
+                        </div>
+                    }
+
+                    {task.attachment &&
+                        <div className="icon-with-counts">
+                            <i className="icon-task-attachments" title="Attachments"></i>
+                            <span>{task.comments.length + 1}</span>
+                        </div>
+                    }
+                    {task.checklists &&
+                        <div className="icon-with-counts">
+                            <i className="icon-checklists" title="Checklists"></i>
+                            {/* <span>{task.checklists.todos?.length + 1}</span> */}
+                        </div>
+                    }
+
+                    {task.dueDate < Date.now() &&
+                        <div className="due-date">
+                            <i className="icon-clock-alert" title="Checklists"></i>
+                            <span>{date[1]} {date[2]} {date[3]}</span>
+                        </div>
+                    }
+                </div>
+                {task.memberIds &&
+                    <div className="members">
+                        <i className="icon-member-gray" title="User name"></i>
+                    </div>}
             </div>
         </div>
     )
