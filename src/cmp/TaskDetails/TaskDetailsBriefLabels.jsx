6d64ed63ea9cea3d3eabb301a3a38d2a7fb11c2f
MeitarGiladi <95337588+MeitarGiladi@users.noreply.github.com>
2024-01-21 23:47:19 +0200

    labels taskDetails

--- src/cmp/TaskDetails/TaskDetailsBriefLabels.jsx
+++ src/cmp/TaskDetails/TaskDetailsBriefLabels.jsx
@@ -1,15 +1,14 @@
 
 
-
 export function TaskDetailsBriefLabels({ boardLabels, taskLabels, cbOpenTaskModal }) {
 
     return <div className="td-labels brief-item">
         <h3>Labels</h3>
         <div className="brief-data">
             {taskLabels.map((labelId, i) => {
-                return <botton key={i}>
-                    {labelId}
-                </botton>
+                return <button key={i} style={{backgroundColor: "silver"}}>
+                    <p>{labelId}</p>
+                </button>
             })}
             <button className="transparent-btn-neutral add-btn" onClick={(ev) => cbOpenTaskModal(ev, "taskLabels")} >
                 <i className="icon-task-plus"></i>
