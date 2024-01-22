ae6bbdabc12969c7cf46b59e6cdb9295c565f219
danyanuka <danyanuka@gmail.com>
2024-01-22 17:38:26 +0200

    ÁppHeader in progress

--- src/cmp/AppDynHeader.jsx
+++ src/cmp/AppDynHeader.jsx
@@ -1,9 +1,35 @@
 import { NavLink, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useColorThief from "use-color-thief";
+import { useDispatch, useSelector } from "react-redux";
+import { utilService } from "../services/util.service";
 
 export function AppDynHeader() {
+  const board = useSelector((storeState) => storeState.boardModule.curBoard);
+  const [headerStyleProps, setHeaderStyleProps] = useState();
+
+  const { color } = useColorThief(utilService.getCleanURL(board), {
+    format: "hex",
+    colorCount: 10,
+    quality: 10,
+  });
+
+  useEffect(() => {
+    setHeaderStyles();
+  }, [board]);
+
+  function setHeaderStyles() {
+    let headerStyles = {};
+    headerStyles.backgroundColor = color;
+    // console.log(headerStyles);
+    setHeaderStyleProps(headerStyles);
+  }
+
+  console.log(board);
   return (
-    <div className="app-header-container">
+    <div style={headerStyleProps} className="app-header-container">
       <div className="app-header-content ">
+        <p>hey</p>
         <Link className="app-header-logo-link transparent-btn-black" to="/">
           <div className="app-header-logo-container"></div>
         </Link>
--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,7 +1,5 @@
-
-import isDarkColor from 'is-dark-color'
-import { FastAverageColor } from 'fast-average-color';
-
+import isDarkColor from "is-dark-color";
+import { FastAverageColor } from "fast-average-color";
 
 export const utilService = {
   padTwo,
@@ -15,8 +13,18 @@ export const utilService = {
   getUserShortName,
   isDarkImg,
   getUserAvatar,
-  toTitleCase
+  toTitleCase,
+  getCleanURL,
 };
+
+function getCleanURL(board) {
+  if (board) {
+    const regex = /url\((.*?)\)/;
+    const imgPath = board?.style?.backgroundImage;
+    const imageUrl = imgPath.match(regex)[1];
+    return imageUrl;
+  }
+}
 
 function padTwo(num) {
   return String(num).padStart(2, "0");
@@ -70,79 +78,77 @@ function calcModalPosition(buttonPos, modalSize) {
 }
 
 function getLabels(labelIds, board) {
-  let labels = []
+  let labels = [];
   labelIds?.map((labelId) => {
-    let label = board.labels?.find(label => labelId === label.id)
-    labels.push(label)
-  })
-  return labels
+    let label = board.labels?.find((label) => labelId === label.id);
+    labels.push(label);
+  });
+  return labels;
 }
 
 function getMembers(memberIds, board) {
-  let members = []
+  let members = [];
   memberIds?.map((id) => {
-    let member = board.members?.find(member => id === member._id)
-    members.push(member)
-  })
-  return members
+    let member = board.members?.find((member) => id === member._id);
+    members.push(member);
+  });
+  return members;
 }
 
 //not working yet
 function checkDueDate(dueDate) {
-  const today = Date.now()
+  const today = Date.now();
   // console.log(today);
-  const tomorrow = Date.now() + 1
+  const tomorrow = Date.now() + 1;
   // console.log(tomorrow);
 
   let dateStatus = {
     isPass: null,
     isToday: null,
-    isTomorrow: null
-  }
+    isTomorrow: null,
+  };
 
   if (dueDate < today) {
-    return { ...dateStatus, isPass: true }
+    return { ...dateStatus, isPass: true };
   }
   if (dueDate === today) {
-    return { ...dateStatus, isToday: true }
+    return { ...dateStatus, isToday: true };
   }
   if (dueDate > tomorrow) {
-    return { ...dateStatus, isTomorrow: true }
+    return { ...dateStatus, isTomorrow: true };
   }
 
-  return dateStatus
+  return dateStatus;
 }
 
 function getStatusChecklist(checklist) {
   let todos;
-  let isDone = []
+  let isDone = [];
 
-  checklist?.map((list) => (
-    todos = list.todos
-  ))
+  checklist?.map((list) => (todos = list.todos));
 
   todos?.map((todo) => {
     if (todo.isDone) {
-      isDone.push(todo)
+      isDone.push(todo);
     }
-  })
+  });
 
   const counts = {
     todos: todos?.length,
-    isDone: isDone.length
-  }
+    isDone: isDone.length,
+  };
 
-  return counts
+  return counts;
 }
 
 function getUserShortName(fullName) {
-  const nameParts = fullName?.split(' ')
+  const nameParts = fullName?.split(" ");
   if (nameParts?.length >= 2) {
-    return (nameParts[0][0] + nameParts[1][0]).toUpperCase()
+    return (nameParts[0][0] + nameParts[1][0]).toUpperCase();
   } else if (nameParts?.length == 1 && nameParts[0].length >= 2) {
-    return nameParts[0].slice(0, 2).toUpperCase()
+    return nameParts[0].slice(0, 2).toUpperCase();
   }
-  return "N/A"
+  return "N/A";
 }
 
 // async function isDarkImg(imgPath) {
@@ -153,15 +159,15 @@ function getUserShortName(fullName) {
 // }
 
 async function isDarkImg(imgPath) {
-  const img = await _loadImage(imgPath)
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  if (img.width == 0) return true
+  const img = await _loadImage(imgPath);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  if (img.width == 0) return true;
   canvas.width = img.width;
   canvas.height = img.height;
-  ctx.drawImage(img, 0, 0)
+  ctx.drawImage(img, 0, 0);
   const rgbaData = ctx.getImageData(0, 0, img.width, img.height).data;
-  canvas.remove()
+  canvas.remove();
 
   let i;
   let sum_r = 0;
@@ -174,32 +180,30 @@ async function isDarkImg(imgPath) {
     sum_b += rgbaData[i + 2];
     sum_a += rgbaData[i + 3];
   }
-  const avg_r = sum_r / (i / 4)
-  const avg_g = sum_g / (i / 4)
-  const avg_b = sum_b / (i / 4)
-  const avg_a = sum_a / (i / 4)  // Convertion of RGBA to RGB isn't needed, because (a == 255)
+  const avg_r = sum_r / (i / 4);
+  const avg_g = sum_g / (i / 4);
+  const avg_b = sum_b / (i / 4);
+  const avg_a = sum_a / (i / 4); // Convertion of RGBA to RGB isn't needed, because (a == 255)
 
   // https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-  // Step 1 
+  // Step 1
   let vR = avg_r / 255;
   let vG = avg_g / 255;
   let vB = avg_b / 255;
 
-  // Step 2 
+  // Step 2
   vR = _sRGBtoLin(vR);
   vG = _sRGBtoLin(vG);
   vB = _sRGBtoLin(vB);
 
   // Step 3
-  const Y = 0.2126 * vR + 0.7152 * vG + 0.0722 * vB
+  const Y = 0.2126 * vR + 0.7152 * vG + 0.0722 * vB;
 
-  // Step 4 
-  const Lstar = YtoLstar(Y)
-  
-  if (Lstar < 50)
-    return true
-  return false
+  // Step 4
+  const Lstar = YtoLstar(Y);
 
+  if (Lstar < 50) return true;
+  return false;
 }
 
 async function _loadImage(src) {
@@ -218,32 +222,29 @@ function _sRGBtoLin(colorChannel) {
   if (colorChannel <= 0.04045) {
     return colorChannel / 12.92;
   } else {
-    return Math.pow(((colorChannel + 0.055) / 1.055), 2.4);
+    return Math.pow((colorChannel + 0.055) / 1.055, 2.4);
   }
 }
 
 function YtoLstar(Y) {
   // Send this function a luminance value between 0.0 and 1.0,
   // and it returns L* which is "perceptual lightness"
-  if (Y <= (216 / 24389)) {       // The CIE standard states 0.008856 but 216/24389 is the intent for 0.008856451679036
-    return Y * (24389 / 27);  // The CIE standard states 903.3, but 24389/27 is the intent, making 903.296296296296296
+  if (Y <= 216 / 24389) {
+    // The CIE standard states 0.008856 but 216/24389 is the intent for 0.008856451679036
+    return Y * (24389 / 27); // The CIE standard states 903.3, but 24389/27 is the intent, making 903.296296296296296
   } else {
-    return Math.pow(Y, (1 / 3)) * 116 - 16;
+    return Math.pow(Y, 1 / 3) * 116 - 16;
   }
 }
 
 function getUserAvatar(user) {
-  const username = user.fullname.split(' ')
-  const firstName = username[0]
-  const lastName = username[1]
+  const username = user.fullname.split(" ");
+  const firstName = username[0];
+  const lastName = username[1];
 }
 
 function toTitleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
-
