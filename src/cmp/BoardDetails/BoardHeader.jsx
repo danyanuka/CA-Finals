f6bb817c899cde26fe80c0813bad6ac77fa77a1e
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2024-01-23 21:04:09 +0200

    tset

--- src/cmp/BoardDetails/BoardHeader.jsx
+++ src/cmp/BoardDetails/BoardHeader.jsx
@@ -1,32 +1,68 @@
 
 import { useSelector } from "react-redux";
 import { UserAvatar } from "../UserAvatar";
+import { utilService } from "../../services/util.service";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 export function BoardHeader() {
+    const [avgColorBg, setAvgColorBg] = useState("#FFFFFF");
+    const [headerStyleProps, setHeaderStyleProps] = useState();
     const board = useSelector(storeState => storeState.boardModule.curBoard)
+    const location = useLocation();
+    const pathname = location.pathname;
 
-    const style = {
-        backgroundColor: board?.style.backgroundColor,
-        opacity: 1.5
+    useEffect(() => {
+        async function setColorAsync() {
+            const avgColor = await getBoardAvgColor(board);
+            setAvgColorBg(avgColor);
+        }
+        setColorAsync();
+    }, [board]);
+
+    useEffect(() => {
+        setHeaderStyles();
+    }, [avgColorBg]);
+
+
+    async function getBoardAvgColor(board) {
+        if (board?.style?.backgroundImage) {
+            const imgPath = board.style.backgroundImage;
+            const cleanImgUrl = utilService.getCleanURL(imgPath);
+            return utilService.getImgAvgColor(cleanImgUrl);
+        } else if (board?.style?.backgroundColor) {
+            const bgColor = board.style.backgroundColor;
+            return new Promise((resolve, reject) => resolve(bgColor));
+        } else {
+            return new Promise((resolve, reject) => resolve("#FFFFFF"));
+        }
+    }
+
+    function setHeaderStyles() {
+        if (pathname === "/board") return;
+        let headerStyles = {};
+        headerStyles.backgroundColor = avgColorBg;
+        headerStyles.opacity = 1.5
+        headerStyles.color = utilService.isDarkColor(avgColorBg, 80)
+            ? "#FFFFFF"
+            : "#000000";
+
+        setHeaderStyleProps(headerStyles);
     }
 
 
+
     return (
-        <div className="board-header" style={style}>
+        <div className="board-header" style={headerStyleProps}>
 
             <div className="board-header-section">
                 <div className="board-name" >
-                    Testing Trello
+                    {board?.title}
                 </div>
 
                 <div className="board-header-btn" title="Click to star or unstar this board. Starred boards show up at the top of your boards list" >
                     <i className="icon icon-star"></i>
-                </div>
-
-                <div className="board-header-btn" title="Change visibility" >
-                    <i className="icon icon-members"></i>
-                    Workspace Visible
                 </div>
 
                 <div className="board-header-btn board-btn" title="Board" >
@@ -34,23 +70,9 @@ export function BoardHeader() {
                     Board
                 </div>
 
-                <div className="board-header-btn" title="Customize views" >
-                    <i className="icon icon-show-more"></i>
-                </div>
-
             </div>
 
             <div className="board-header-section">
-
-                <div className="board-header-btn" title="Power-Ups">
-                    <i className="icon icon-power"></i>
-                    Power-Ups
-                </div>
-
-                <div className="board-header-btn" title="automation" >
-                    <i className="icon icon-automation"></i>
-                    Automation
-                </div>
 
                 <div className="board-header-btn" title="Filter cards" >
                     <i className="icon icon-filter"></i>
@@ -68,19 +90,10 @@ export function BoardHeader() {
                     </div>}
 
                 <div className="board-header-btn share-btn" title="Share board" >
-                    <i className="icon icon-account"></i>
-                    Share
+                    <i className="icon-account"></i>
+                    <span>Share</span>
                 </div>
-
-                <div className="board-header-btn" title="Share board" >
-                    <i className="icon icon-side-bar"></i>
-                </div>
-
-
-
             </div>
-
-
         </div>
     )
 }
