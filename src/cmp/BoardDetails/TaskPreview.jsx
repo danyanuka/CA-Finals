04df9a07ce3fc535ef91e39ef04fd9ee81c6ea4b
meitarg <meitarg@checkpoint.com>
2024-01-24 18:40:51 +0200

    adding label color classes

--- src/cmp/BoardDetails/TaskPreview.jsx
+++ src/cmp/BoardDetails/TaskPreview.jsx
@@ -74,9 +74,8 @@ export function TaskPreview({ task, index }) {
                   return (
                     <div
                       key={label.id}
-                      className="label"
+                      className={`label base-bg-color-${label.color}`}
                       title={`title: ${label.title}`}
-                      style={{ backgroundColor: label.color }}
                     ></div>
                   );
                 })}
